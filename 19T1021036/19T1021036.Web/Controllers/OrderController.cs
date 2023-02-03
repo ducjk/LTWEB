e62b1e46594bbb2ea2c6638705876c799efb5e24
ducjk <duchuynh360@gmail.com>
2023-02-03 15:08:23 +0700

    push code

---
+++ 19T1021036/19T1021036.Web/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _19T1021036.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ 19T1021036/19T1021036.Web/Controllers/SupplierController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _19T1021036.Web.Controllers
+{
+    public class SupplierController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
